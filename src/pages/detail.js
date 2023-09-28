750260eb6a5ae2195c59946b80caebc7fae4ff75
Takuzen Toh <takuzen0430@gmail.com>
2023-09-28 22:31:17 +0900

    chore: detail.js

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,35 +1,62 @@
 import { Inter } from 'next/font/google'
+import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Detail() {
-    return (
-      <main
-        className={`flex min-h-[100vh] flex-col items-center p-24 gap-10 ${inter.className}`}
-      >
-        <div className="font-bold font-serif text-4xl">
-          <h1>
-            Book Cube
-          </h1>
+export default function Home() {
+  return (
+    <main
+      className={`flex flex-wrap min-h-[100vh] max-w-[100vw] gap-5 items-center justify-center p-24 ${inter.className}`}
+    >
+      <div className="">
+        <Image
+          src="/book-sample-0.png"
+          alt="book-sample-0"
+          width={300}
+          height={315}>
+        </Image>
         </div>
-        
-        <div className="z-10" style={{ width: '500px', height: '500px' }}>
-          <model-viewer
-            style={{ width: '500px', height: '500px' }}
-            src="/scene.glb"
-            width="500px"
-            height="500px"
-            alt="A 3D model of a table with food on it"
-            ar
-            shadow-intensity="1"
-            camera-controls
-            touch-action="pan-y">  
-          </model-viewer>
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Button
-          </button>
+        <div className="">
+        <Image
+          src="/book-sample-1.png"
+          alt="book-sample-1"
+          width={300}
+          height={315}>
+        </Image>
         </div>
-        
-      </main>
-    )
-  }
+        <div className="">
+        <Image
+          src="/book-sample-2.png"
+          alt="book-sample-2"
+          width={300}
+          height={315}>
+        </Image>
+        </div>
+        <div className="">
+        <Image
+          src="/book-sample-3.png"
+          alt="book-sample-3"
+          width={300}
+          height={315}>
+        </Image>
+        </div>
+        <div className="">
+        <Image
+          src="/book-sample-4.png"
+          alt="book-sample-4"
+          width={300}
+          height={315}>
+        </Image>
+        </div>
+        <div className="">
+        <Image
+          src="/book-sample-5.png"
+          alt="book-sample-5"
+          width={300}
+          height={315}>
+        </Image>
+        </div>
+
+    </main>
+  )
+}
