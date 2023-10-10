a456739369bd65935b86359ebb894c5acec76ebd
Takuzen Toh <takuzen0430@gmail.com>
2023-10-10 12:06:27 +0900

    feat: update the weekly cube

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,12 +51,24 @@ export default function Home() {
           )}
         </div>
         </section>
-        <section id="weekly-pick" className="z-10 gap-7 flex flex-col justify-center bg-[#d7ecea] w-[100vw]">
+        <section id="weekly-pick" className="z-10 py-5 gap-7 flex flex-col justify-center bg-[#d7ecea] w-[100%]">
           <div className='self-center'>
             <p className='text-[#DB4D6D] font-black font-serif text-xl'>Weekly Pick</p>
           </div>
           <div className='self-center'> 
-            { isClient ? <model-viewer src="/scene.glb" alt="Your 3D model" width="300px" height="300px" auto-rotate rotation-per-second="30deg" camera-controls camera-orbit="45deg 55deg 4m" shadow-intensity="1"></model-viewer> : 'Loading model...' }
+            { isClient ? 
+            <model-viewer
+              style={{ height: '300px' }}
+              src="https://firebasestorage.googleapis.com/v0/b/booksp-eae3c.appspot.com/o/users%2FNupoAf7FpgPdbxVH2p9VitTJnWj1%2Fcubes%2FHugo%202023%20Nominees%2FHugo%202023%20Nominees.glb?alt=media&token=d42d6e6f-37eb-4b97-824d-7a92170cdf57"
+              alt="Your 3D model"
+              width="200px"
+              height="200px"
+              auto-rotate
+              rotation-per-second="30deg"
+              camera-controls
+              camera-orbit="45deg 55deg 20m"
+              shadow-intensity="1"></model-viewer>
+              : 'Loading model...' }
           </div>
           <div className="flex flex-col gap-7">
           <div className='self-center'>
@@ -64,10 +76,13 @@ export default function Home() {
               <p>@booksp.offcial</p>
             </Link>
           </div>
-          <div className="">  
+          <div className="self-center w-[500px]">  
             <p className="">
-              These six books are selected for 2023 Hugo Award, which is the most prestigious award for science fiction and fantasy. The announcement of the winner will be on October 21th.
-              <span className='text-gray-400'>read more ...</span>
+              These six books are selected for 2023 Hugo Award, which is the most prestigious award for science fiction and fantasy.
+            </p>
+            <br />
+            <p>
+              The announcement of the winner will be on October 21th.
             </p>
           </div>
           <div className="self-center flex flex-col justify-center">
